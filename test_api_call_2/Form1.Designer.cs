25178975f7d24181f49ea044d3534e18d6bdd337
Ali <aliroshan@live.com>
2014-04-08 23:11:15 +0500

    console code ported completely to gui version. Loads Event Types into list box.

--- test_api_call_2/Form1.Designer.cs
+++ test_api_call_2/Form1.Designer.cs
@@ -29,33 +29,55 @@
         private void InitializeComponent()
         {
             this.btnLoadEventTypes = new System.Windows.Forms.Button();
+            this.lbEventTypes = new System.Windows.Forms.ListBox();
+            this.tbException = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnLoadEventTypes
             // 
             this.btnLoadEventTypes.Location = new System.Drawing.Point(154, 42);
             this.btnLoadEventTypes.Name = "btnLoadEventTypes";
-            this.btnLoadEventTypes.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadEventTypes.Size = new System.Drawing.Size(114, 23);
             this.btnLoadEventTypes.TabIndex = 0;
             this.btnLoadEventTypes.Text = "Load Event Types";
             this.btnLoadEventTypes.UseVisualStyleBackColor = true;
             this.btnLoadEventTypes.Click += new System.EventHandler(this.btnLoadEventTypes_Click);
+            // 
+            // lbEventTypes
+            // 
+            this.lbEventTypes.FormattingEnabled = true;
+            this.lbEventTypes.Location = new System.Drawing.Point(29, 80);
+            this.lbEventTypes.Name = "lbEventTypes";
+            this.lbEventTypes.Size = new System.Drawing.Size(370, 381);
+            this.lbEventTypes.TabIndex = 1;
+            // 
+            // tbException
+            // 
+            this.tbException.Location = new System.Drawing.Point(29, 467);
+            this.tbException.Name = "tbException";
+            this.tbException.Size = new System.Drawing.Size(370, 20);
+            this.tbException.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 503);
+            this.Controls.Add(this.tbException);
+            this.Controls.Add(this.lbEventTypes);
             this.Controls.Add(this.btnLoadEventTypes);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnLoadEventTypes;
+        private System.Windows.Forms.ListBox lbEventTypes;
+        private System.Windows.Forms.TextBox tbException;
     }
 }
 
--- test_api_call_2/Form1.cs
+++ test_api_call_2/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using test_api_call_2.TO;
+using test_api_call_2.Json;
 
 namespace test_api_call_2
 {
     public partial class Form1 : Form
     {
         private static string Url = "https://api.betfair.com/exchange/betting";
+        private static string key = "RRAy2ZiLvI9Mqyoe";
+        private static string tok = "Err6jG623XOtEf6BdPQv/YCLSYsI5zUB9Y5XgLqOdeA=";
 
         public Form1()
         {
@@ -21,7 +25,33 @@ namespace test_api_call_2
 
         private void btnLoadEventTypes_Click(object sender, EventArgs e)
         {
+            IClient client = null;
 
+            client = new JsonRpcClient(Url, key, tok);
+
+            try
+            {
+                var marketFilter = new MarketFilter();
+
+                var eventTypes = client.listEventTypes(marketFilter);
+
+                // forming a eventype id set for the eventype id extracted from the result
+                ISet<string> eventypeIds = new HashSet<string>();
+
+                foreach (EventTypeResult eventType in eventTypes)
+                {
+                    lbEventTypes.Items.Add(eventType.EventType.Name);
+                    eventypeIds.Add(eventType.EventType.Id);
+                }
+            }
+            catch (APINGException apiException)
+            {
+                tbException.Text = "Got an exception from Api-NG: " + apiException.ErrorCode;
+            }
+            catch (System.Exception se)
+            {
+                tbException.Text = "Unknown exception from application: " + se.Message;
+            }
         }
     }
 }
